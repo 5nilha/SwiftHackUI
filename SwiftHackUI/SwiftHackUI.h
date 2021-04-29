aa74c37efe38129fc7ac9eaf102b4e0cc72c57f0
Fabio <bwtdirect@gmail.com>
2021-04-29 01:45:04 -0300

    Initial Commit

---
+++ SwiftHackUI/SwiftHackUI.h
@@ -0,0 +1,18 @@
+//
+//  SwiftHackUI.h
+//  SwiftHackUI
+//
+//  Created by Fabio Quintanilha on 4/29/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftHackUI.
+FOUNDATION_EXPORT double SwiftHackUIVersionNumber;
+
+//! Project version string for SwiftHackUI.
+FOUNDATION_EXPORT const unsigned char SwiftHackUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHackUI/PublicHeader.h>
+
+
